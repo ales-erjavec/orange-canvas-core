b24f13a4b401dc5111bb08354b8fb8c825f2d33b
Ales Erjavec <ales.erjavec@fri.uni-lj.si>
2019-05-10 17:33:12 +0200

    application: Move macos_set_nswindow_tabbing call to application

--- orangecanvas/application/application.py
+++ orangecanvas/application/application.py
@@ -2,21 +2,22 @@
 Orange Canvas Application
 
 """
+from orangecanvas.gui.utils import macos_set_nswindow_tabbing
 
 from AnyQt.QtWidgets import QApplication
-
 from AnyQt.QtCore import Qt, QUrl, QEvent, pyqtSignal as Signal
 
 
 class CanvasApplication(QApplication):
     fileOpenRequest = Signal(QUrl)
 
-    def __init__(self, argv):
+    def __init__(self, argv, **kwargs):
         if hasattr(Qt, "AA_EnableHighDpiScaling"):
             # Turn on HighDPI support when available
             QApplication.setAttribute(Qt.AA_EnableHighDpiScaling)
-        super().__init__(argv)
+        super().__init__(argv, **kwargs)
         self.setAttribute(Qt.AA_DontShowIconsInMenus, True)
+        macos_set_nswindow_tabbing(False)
 
     def event(self, event):
         if event.type() == QEvent.FileOpen:
